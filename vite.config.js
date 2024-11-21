d9906833f54f7217b9c128f02b392ab133ba12fb
dimpurr <dimpurr@live.com>
2024-11-21 16:26:14 +0000

    no base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/kcl-archive-guide/',
+  // base: '/kcl-archive-guide/',
   plugins: [react()]
 })
